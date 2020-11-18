c3bd818c1d66ce58f26d321bcaa081ef3f504814
lucasfernande <lucas.fernandes6192@outlook.com>
2020-11-18 14:26:18 -0300

    requisitando as páginas documentação e suporte para o backend

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-$(document).ready(() => {
-	
+// executa todas as ações apenas após o carregamento do DOM
+$(document).ready(() => { 
+	$('#documentacao').on('click', () => {
+		$('#pagina').load('documentacao.html') // fazendo request do conteudo do arquivo documentacao.html para substituir o conteudo da div pagina
+	})
+
+	$('#suporte').on('click', () => {
+		$('#pagina').load('suporte.html')
+	})
 })
