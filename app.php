80f0e6df3a168889bcae71038ba7bc04e76929ae
lucasfernande <lucas.fernandes6192@outlook.com>
2020-11-18 17:30:09 -0300

    listando os dados com o método $.ajax()

--- app.php
+++ app.php
@@ -5,6 +5,9 @@
 		private $dataFim;
 		private $numeroVendas;
 		private $totalVendas;
+		private $clientesAtivos;
+		private $clientesInativos;
+		private $totalDespesas;
 
 		public function __get($attr) {
 			return $this->$attr;
@@ -13,6 +16,12 @@
 		public function __set($attr, $value) {
 			$this->$attr = $value;
 		}
+
+		# método para criar um json no retorno da requisição mesmo com os atributos sendo private
+		public function criarJson() {
+            $jsonDashboard = json_encode(get_object_vars($this)); 
+            return $jsonDashboard;
+        }
 	}
 
 	# classe de conexão com a database
@@ -64,16 +73,54 @@
 
 			return $statemt->fetch(PDO::FETCH_OBJ)->totalVendas; # retornando apenas o totalVendas, e não o objeto;
 		}
+
+		public function getClientesAtivos() {
+			$query = 'select count(*) as clientesAtivos from tb_clientes where cliente_ativo = 1';
+
+			$statemt = $this->conn->prepare($query);
+			$statemt->execute();
+
+			return $statemt->fetch(PDO::FETCH_OBJ)->clientesAtivos;
+		}
+
+		public function getClientesInativos() {
+			$query = 'select count(*) as clientesInativos from tb_clientes where cliente_ativo = 0';
+
+			$statemt = $this->conn->prepare($query);
+			$statemt->execute();
+
+			return $statemt->fetch(PDO::FETCH_OBJ)->clientesInativos;
+		}
+
+		public function getTotalDespesas() {
+			$query = 'select SUM(total) as totalDespesas from tb_despesas where data_despesa BETWEEN :dataInicio AND :dataFim';
+
+			$statemt = $this->conn->prepare($query);
+			$statemt->bindValue(':dataInicio', $this->dashboard->__get('dataInicio'));
+			$statemt->bindValue(':dataFim', $this->dashboard->__get('dataFim'));
+			$statemt->execute();
+
+			return $statemt->fetch(PDO::FETCH_OBJ)->totalDespesas;
+		}
 	}
 
 	$conn = new Conn();
 	$dashboard = new Dashboard();
 	$bd = new BD($conn, $dashboard);
 
-	$dashboard->__set('dataInicio', '2020-08-01'); // teste
-	$dashboard->__set('dataFim', '2020-08-31'); // teste
+	$competencia = explode('-', $_GET['competencia']); // separando ano e mes para calcular o ultimo dia deste mes 
 
+	$ano = $competencia[0];
+	$mes = $competencia[1];
+
+	$diasMes = cal_days_in_month(CAL_GREGORIAN, $mes, $ano); // calculando quantos dias tem determinado mês (28, 30 ou 31)
+
+	$dashboard->__set('dataInicio',  $_GET['competencia'].'-01');
+	$dashboard->__set('dataFim', $_GET['competencia'].'-'.$diasMes); //	formando a data final com o ultimo dia do mes
 	$dashboard->__set('numeroVendas', $bd->getNumeroVendas());
 	$dashboard->__set('totalVendas', $bd->getTotalVendas());
-	print_r($dashboard);
+	$dashboard->__set('clientesAtivos', $bd->getClientesAtivos());
+	$dashboard->__set('clientesInativos', $bd->getClientesInativos());
+	$dashboard->__set('totalDespesas', $bd->getTotalDespesas());
+	echo $dashboard->criarJson();
 ?>
--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 // executa todas as ações apenas após o carregamento do DOM
 $(document).ready(() => { 
+
 	$('#documentacao').on('click', () => {
 		$('#pagina').load('documentacao.html') // fazendo request do conteudo do arquivo documentacao.html para substituir o conteudo da div pagina
 	})
 
 	$('#suporte').on('click', () => {
 		$('#pagina').load('suporte.html')
+	})
+
+	// recuperando valor selecionado no campo competência e mandando para o backend
+	$('#competencia').on('change', e => {
+
+		let competencia = $(e.target).val()
+
+		$.ajax({ 
+			type: 'GET', // método
+			url: 'app.php', // url (para onde vai)
+			data: `competencia=${competencia}`, // dados necessários
+			dataType: 'json', // definindo que a resposta deve ser em um objeto literal json
+			success: response => {  // o que acontece em caso de sucesso
+				$('#numeroVendas').html(response.numeroVendas) 
+				$('#totalVendas').html(response.totalVendas)
+				$('#clientesAtivos').html(response.clientesAtivos)
+				$('#clientesInativos').html(response.clientesInativos)
+				$('#totalDespesas').html(response.totalDespesas)
+			},
+			error: error => { // o que acontece em caso de erro
+				$('#numeroVendas').html('') 
+				$('#totalVendas').html('')
+				$('#clientesAtivos').html('')
+				$('#clientesInativos').html('')
+				$('#totalDespesas').html('')
+			} 
+		})
 	})
 })
